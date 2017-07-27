ded828d000091393baab425f568f3443a3867afa
Matt Roberts <matt.roberts2@digital.hmrc.gov.uk>
2017-07-27 17:19:07 +0100

    WIP

--- app/controllers/ItemManagementController.scala
+++ app/controllers/ItemManagementController.scala
@@ -21,24 +21,20 @@ class ItemManagementController @Inject()(val messagesApi: MessagesApi, environme
 
   def createItem: Action[MultipartFormData[Files.TemporaryFile]] = Action(parse.multipartFormData) { implicit request =>
     val postAction = request.body.asFormUrlEncoded.get("action").get(0)
-
-
     val boundForm = ItemData.createItemForm.bindFromRequest
     boundForm.fold({ formWithErrors =>
       BadRequest(views.html.items(ItemData.items, formWithErrors))
     }, { itemData =>
-
       request.body.file("picture").map { picture =>
         import java.io.File
         val filename = picture.filename.replaceAll(" ", "_")
         val p = environment.rootPath.getAbsolutePath
         val path = s"$p/public/images/$filename"
         picture.ref.moveTo(new File(path))
-
-
         val item = Item(itemData.name, itemData.description, itemData.manufacturer,
           itemData.warranty, itemData.price, itemData.discount, itemData.seller,
           routes.Assets.at(s"images/$filename").url)
+
 
         if(itemData.index.get != -1 && postAction == "save") {
           ItemData.items.update(itemData.index.get, item)
@@ -47,14 +43,10 @@ class ItemManagementController @Inject()(val messagesApi: MessagesApi, environme
         } else {
           ItemData.items.append(item)
         }
-
-
       }.getOrElse {
         Redirect(routes.Application.index).flashing(
           "error" -> "Missing file")
       }
-
-
       Redirect(routes.ItemManagementController.listItems)
     })
   }
--- build.sbt
+++ build.sbt
@@ -10,6 +10,8 @@ scalaVersion := "2.11.11"
 
 libraryDependencies ++= Seq( jdbc , cache , ws , specs2 % Test )
 
-unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/bscontactform" )
+libraryDependencies += "org.reactivemongo" %% "play2-reactivemongo" % "0.12.5-play25"
+
+unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/" )
 
       
